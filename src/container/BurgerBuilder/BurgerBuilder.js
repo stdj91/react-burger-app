6416702d2932439924a09ffeaf13d25fbc118e10
unknown <stdj.1907@gmail.com>
2020-05-22 16:19:04 +0200

    burger ingredients show in browser

--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/AuxProblem'
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7,
+}
+
 class BurgerBuilder extends Component {
    /* constructor (props) {
         super(props);
@@ -15,13 +22,35 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0,
-        }
+        },
+        totalPrice: 4
     }
+
+    addIngredientsHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updateCounted = oldCount + 1;
+        const updateIngredients = {
+            ...this.state.ingredients
+        }
+        updateIngredients[type] = updateCounted;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState ({totalPrice: newPrice, ingredients: updateIngredients})
+
+    }
+
+    removeIngredientsHandler = (type) => {
+
+    }
+
+
     render () {
         return (
            <Aux>
                <Burger ingredients={this.state.ingredients}/>
-               <BuildControls />
+               <BuildControls 
+                ingredientAdded={this.addIngredientsHandler} />
            </Aux>
         );
     }
