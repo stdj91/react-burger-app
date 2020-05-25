50d31ee4f78071eb4689aff39f8b42332b3f237b
unknown <stdj.1907@gmail.com>
2020-05-25 20:17:20 +0200

    commit before merge

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder'
 
 
 class App extends Component  {
+ 
   render () {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <BurgerBuilder /> 
         </Layout>
         </div>
       );
